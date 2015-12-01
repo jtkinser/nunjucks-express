a5c606a8faacc090571850d17142da087c8d64b9
Corban Baxter <corbanb@gmail.com>
2015-12-01 13:35:57 -0700

    adding more info

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nunjucks + Express Demo App
+
+This project is very straigtforward and only intended to show how to setup nunjucks. Potentially used to demo bugs to the Mozilla team in the future.
+
+- Created By: [Corban Baxter](https://github.com/corbanb)
--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 var express = require('express');
-var nunjucks = require('nunjucks');
 var app = express();
 
+// INSTALL Nunjucks
+var nunjucks = require('nunjucks');
+
+// Create nunjucks enviorment
 var nunjucksEnv = nunjucks.configure('./views', { express: app });
 
-// simple filter method for logging in the views
+// DEMO: add simple filter method for logging in the views
 nunjucksEnv.addFilter('log', console.log);
 
+// Tell express which engine to use
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
 app.get('/', function (req, res) {
   var data = {
-      title: 'Demo Express App + Nunjucks',
+      title: 'Express + Nunjucks Demo App',
       info: {
-          title: 'Nunjucks + Express',
+          title: 'Express + Nunjucks',
           description: 'Simple app showing how to setup nunjucks for an express.js app.'
       }
   };
+
+  // render page with nunjucks
+  // INFO: See views/layout.html and views/main/index.html for templates
   res.render('main/index', data);
 });
 
